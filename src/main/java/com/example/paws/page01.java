dc330689722c502bc1fb2371b7254f93d1f06dbd
prasantheif <133153318+prasantheif@users.noreply.github.com>
2023-11-26 18:26:44 +0530

    Add files via upload

---
+++ src/main/java/com/example/paws/page01.java
@@ -0,0 +1,14 @@
+package com.example.paws;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class page01 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.page01);
+    }
+}
---
+++ src/main/java/com/example/paws/page3.java
@@ -0,0 +1,14 @@
+package com.example.paws;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class page3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.page3);
+    }
+}
